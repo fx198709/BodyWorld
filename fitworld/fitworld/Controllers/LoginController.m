9c55cfe349d5e0024cf426410a8dbf69b7199cd5
feixiang <feixiang>
2021-11-12 04:05:41 +0800

    替换login

--- fitworld/fitworld/Controllers/LoginController.m
+++ fitworld/fitworld/Controllers/LoginController.m
@@ -7,21 +7,24 @@
 
 #import "AppDelegate.h"
 #import "LoginController.h"
-#import "MainViewController.h"
-#import "AFNetworking.h"
+#import "RegisterController.h"
+
 #import "FITAPI.h"
 #import "UIDeps.h"
 #import "UserInfo.h"
-#import "MainViewController.h"
+
 #import "APPObjOnce.h"
 
 @interface LoginController ()
 
-@property (nonatomic, strong) UITextField *userNameTxt;
-@property (nonatomic, strong) UITextField *passwordTxt;
-@property (nonatomic, strong) UIImageView *inputImageView;
-@property (nonatomic, strong) UIPickerView *countryPicker;
-@property (nonatomic, strong) NSArray *pickerArray;
+@property (weak, nonatomic) IBOutlet UIButton *changeLanguageBtn;
+@property (weak, nonatomic) IBOutlet UIButton *registerBtn;
+
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *pwdField;
+@property (weak, nonatomic) IBOutlet UIButton *showPwdBtn;
+
+@property (weak, nonatomic) IBOutlet UIButton *loginBtn;
 
 
 
@@ -31,208 +34,116 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    [self initView];
+    [self reloadTextView];
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    self.navigationItem.leftBarButtonItem = nil;
+}
+
+- (void)initView {
+    [self.nameField cornerHalfWithBorderColor:[self.nameField backgroundColor]];
+    [self.pwdField cornerHalfWithBorderColor:[self.pwdField backgroundColor]];
+    [self.loginBtn cornerHalfWithBorderColor:[self.loginBtn backgroundColor]];
+}
+
+
+- (void)reloadTextView {
+    [self.loginBtn setTitle:ChineseStringOrENFun(@"登录", @"Login") forState:UIControlStateNormal];
+    [self.registerBtn setTitle:ChineseStringOrENFun(@"免密登录", @"SMS Code Login") forState:UIControlStateNormal];
     
-    UIImageView *bgImgView = [[UIImageView alloc] initWithFrame:self.view.bounds];
-    bgImgView.image = [UIImage imageNamed:@"loginbg"];
-    [self.view insertSubview: bgImgView atIndex:0];
+    NSDictionary *attr = @{NSForegroundColorAttributeName:[UIColor lightGrayColor]};
+    self.nameField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:ChineseStringOrENFun(@"请输入账号", @"Please enter the account number") attributes:attr];
+    self.pwdField.attributedPlaceholder = [[NSAttributedString alloc] initWithString:ChineseStringOrENFun(@"请输入密码", @"Please input a password") attributes:attr];
     
-    UIView *inputBgView = [[UIView alloc] initWithFrame:CGRectZero];
-    [inputBgView setBackgroundColor:[UIColor colorWithRed:0.0 green:0.0 blue:0.0 alpha:0.8]];
-    [inputBgView.layer setMasksToBounds:YES];
-    [inputBgView.layer setCornerRadius:9.0];
-    
-    [bgImgView addSubview: inputBgView];
-    [inputBgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.view).offset(150);
-        make.width.mas_equalTo(300);
-        make.height.mas_equalTo(150);
-        make.centerX.equalTo(self.view);
-    }];
-    
-    UIImageView *logoImgView = [[UIImageView alloc] init];
-    logoImgView.image = [UIImage imageNamed:@"logo_login"];
-    [bgImgView addSubview:logoImgView];
-    [logoImgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.bottom.equalTo(inputBgView.mas_top).offset(-10);
-        make.left.equalTo(inputBgView);
-        make.width.height.mas_equalTo(40);
-    }];
-    
-    
-    
-    UIImageView *pickerLeftImgView = [[UIImageView alloc] init];
-    pickerLeftImgView.image = [UIImage imageNamed:@"login_world"];
-    [inputBgView addSubview:pickerLeftImgView];
-    [pickerLeftImgView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(inputBgView.mas_left).offset(10);
-        make.top.equalTo(inputBgView.mas_top).offset(10);
-        make.width.height.mas_equalTo(15);
-    }];
-    
-    self.pickerArray = [NSArray arrayWithObjects:@"China",@"HongKong",@"Switzerland", nil];
-    self.countryPicker = [[UIPickerView alloc] init];
-    self.countryPicker.delegate = self;
-    self.countryPicker.dataSource = self;
-    
-    [self.view addSubview: self.countryPicker];
-    [self.countryPicker mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.right.equalTo(inputBgView);
-        make.left.equalTo(pickerLeftImgView.mas_right).offset(10);
-        make.height.mas_equalTo(50);
-        make.centerY.equalTo(pickerLeftImgView);
-    }];
-    
-    self.userNameTxt = [[UITextField alloc] init];
-    NSAttributedString *placeholderString = [[NSAttributedString alloc] initWithString:@"mobile number" attributes:
-        @{NSForegroundColorAttributeName:[UIColor whiteColor],
-                     NSFontAttributeName: self.userNameTxt.font
-             }];
-    self.userNameTxt.attributedPlaceholder = placeholderString;
-    self.userNameTxt.textColor = UIColor.whiteColor;
     // TODO 默认账号
-    self.userNameTxt.text = @"+86:18600411689";
-    [self.view addSubview: self.userNameTxt];
-    [self.userNameTxt mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(inputBgView);
-        make.top.equalTo(self.countryPicker.mas_bottom).offset(5);
-        make.height.mas_equalTo(50);
-    }];
-    
-    UIImageView *loginNameFieldLeftView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 0, 15, 15)];
-    loginNameFieldLeftView.image = [UIImage imageNamed:@"login_phone"];
-    UIView *loginNameLeftPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 40, 15)];
-    [loginNameLeftPaddingView addSubview:loginNameFieldLeftView];
-    self.userNameTxt.leftView = loginNameLeftPaddingView;
-    self.userNameTxt.leftViewMode = UITextFieldViewModeAlways;
-    
-    self.passwordTxt = [[UITextField alloc] init];
-    NSAttributedString *placeholderString2 = [[NSAttributedString alloc] initWithString:@"password" attributes:
-        @{NSForegroundColorAttributeName:[UIColor whiteColor],
-                     NSFontAttributeName: self.passwordTxt.font
-             }];
-    self.passwordTxt.attributedPlaceholder = placeholderString2;
-    self.passwordTxt.textColor = UIColor.whiteColor;
+        self.nameField.text = @"+86:13501173505";
     // TODO 默认密码
-    self.passwordTxt.text = @"1122";
-    [self.view addSubview: self.passwordTxt];
-    [self.passwordTxt mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(inputBgView);
-        make.top.equalTo(self.userNameTxt.mas_bottom).offset(5);
-        make.height.mas_equalTo(50);
-    }];
-    
-    UIImageView *pwdFieldLeftView = [[UIImageView alloc]initWithFrame:CGRectMake(10, 0, 15, 15)];
-    pwdFieldLeftView.image = [UIImage imageNamed:@"login_pwd"];
-    UIView *pwdLeftPaddingView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 40, 15)];
-    [pwdLeftPaddingView addSubview:pwdFieldLeftView];
-    self.passwordTxt.leftView = pwdLeftPaddingView;
-    self.passwordTxt.leftViewMode = UITextFieldViewModeAlways;
-    
-    UIButton* loginBtn = [[UIButton alloc] init];
-    [loginBtn setTitle:@"LOGIN" forState:UIControlStateNormal] ;
-    [loginBtn setBackgroundImage:[UIImage imageNamed: @"bg_loginbtn"] forState:UIControlStateNormal];
-    loginBtn.alpha = 0.8;
-    loginBtn.titleLabel.textColor = UIColor.whiteColor;
-    [self.view addSubview:loginBtn];
-    [loginBtn mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(inputBgView);
-        make.top.equalTo(inputBgView.mas_bottom).offset(10);
-        make.height.mas_equalTo(45);
-    }];
-    [loginBtn addTarget: self action: @selector(loginBtnClick) forControlEvents: UIControlEventTouchDown];
-    float startx = ScreenWidth- 150-(ScreenWidth-300)/2-10;
-    UIButton *vbtn = [[UIButton alloc] initWithFrame:CGRectMake(startx, 100, 150, 40)];
-    [self.view addSubview:vbtn];
-    [vbtn setTitle:@"中文|English" forState:UIControlStateNormal];
-    [vbtn setTitle:@"中文|English" forState:UIControlStateHighlighted];
-    vbtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
-    vbtn.titleLabel.font = SystemFontOfSize(20);
-    vbtn.titleLabel.textColor = [UIColor whiteColor];
-    [vbtn addTarget:self action:@selector(changeLanguageBtnClick) forControlEvents:UIControlEventTouchUpInside];
-    vbtn.backgroundColor = [UIColor redColor];
-    [self.view bringSubviewToFront:vbtn];
-    
-    vbtn = [[UIButton alloc] initWithFrame:CGRectMake(startx, 170, 150, 50)];
-    [self.view addSubview:vbtn];
-    [vbtn setTitle:@"免密登录" forState:UIControlStateNormal];
-    [vbtn setTitle:@"免密登录" forState:UIControlStateHighlighted];
-    vbtn.contentHorizontalAlignment = UIControlContentHorizontalAlignmentRight;
-    vbtn.titleLabel.font = SystemFontOfSize(30);
-    vbtn.titleLabel.textColor = [UIColor whiteColor];
-    [vbtn addTarget:self action:@selector(changeLanguageBtnClick) forControlEvents:UIControlEventTouchUpInside];
-    vbtn.backgroundColor = [UIColor redColor];
-    [self.view bringSubviewToFront:vbtn];
+        self.pwdField.text = @"1122";
 }
 
-- (void)changeLanguageBtnClick{
+#pragma mark - action
+
+//修改语言
+- (IBAction)ChangeLanguage:(id)sender {
+    if (ISChinese()) {
+        [ConfigManager sharedInstance].language = LanguageEnum_English;
+    } else {
+        [ConfigManager sharedInstance].language = LanguageEnum_Chinese;
+    }
     
+    //保存当前语言
+    [[ConfigManager sharedInstance] saveConfig];
+    [self reloadTextView];
 }
 
 
+- (IBAction)goToRegister:(id)sender {
+    RegisterController *nextVC = VCBySBName(@"RegisterController");
+    [self.navigationController setViewControllers:[NSArray arrayWithObject:nextVC] animated:YES];
+}
 
-- (void)loginBtnClick{
+
+//点击显示密码
+- (IBAction)clickShowPwd:(id)sender {
+    [self.showPwdBtn setSelected:!self.showPwdBtn.isSelected];
+    [self.pwdField setSecureTextEntry:!self.showPwdBtn.isSelected];
+}
+
+
+- (IBAction)clickLogin {
+    NSString *name = self.nameField.text;
+    NSString *pwd = self.pwdField.text;
+    
+    if ([NSString isNullString:name] || [NSString isNullString:pwd]) {
+        [MTHUD showDurationNoticeHUD:ChineseStringOrENFun(@"用户名和密码不能为空", @"Account and password can not be emtpy")];
+        return;
+    }
+    
     NSString *strUrl = [NSString stringWithFormat:@"%@login", FITAPI_HTTPS_PREFIX];
     NSLog(@"uuid>>>>%@", [UIDevice currentDevice].identifierForVendor.UUIDString);
+    [MTHUD showLoadingHUD];
     AFHTTPSessionManager *manager =[AFHTTPSessionManager manager];
-        NSDictionary *dict = @{
-                               @"username":self.userNameTxt.text,
-                               @"password":self.passwordTxt.text
-                           };
-        [manager POST:strUrl parameters:dict headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
-            // 进度
-        } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
-            NSLog(@"请求成功---%@", responseObject);
-            if ([responseObject objectForKey:@"status"] && [[responseObject objectForKey:@"status"] longLongValue] == 0) {
-                UserInfo *userInfo = [[UserInfo alloc] initWithJSON:responseObject[@"recordset"][@"user"]];
-                userInfo.msg = responseObject[@"recordset"][@"msg"];
-                userInfo.msg_cn = responseObject[@"recordset"][@"msg_cn"];
-                NSString *userToken = responseObject[@"recordset"][@"token"];
-                if(userToken != nil){
-                    [[NSUserDefaults standardUserDefaults] setObject:userToken forKey:@"userToken"];
-                }
-                UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
-                UINavigationController *vc = (UINavigationController *)[storyboard instantiateViewControllerWithIdentifier:@"mainNavVC"];
-                MainViewController *mainvc = [vc.viewControllers firstObject];
-                mainvc.userInfo = userInfo;
-                [APPObjOnce sharedAppOnce].currentUser = userInfo;
-                [self.view.window setRootViewController:vc];
-
-            }else{
-                [CommonTools showAlertDismissWithContent:[responseObject objectForKey:@"msg"] showWaitTime:0 afterDelay:2 control:self];
+    NSDictionary *dict = @{
+        @"username":name,
+        @"password":pwd
+    };
+    [manager POST:strUrl parameters:dict headers:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+        // 进度
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        [MTHUD hideHUD];
+        NSLog(@"请求成功---%@", responseObject);
+        if ([responseObject objectForKey:@"status"] && [[responseObject objectForKey:@"status"] longLongValue] == 0) {
+            UserInfo *userInfo = [[UserInfo alloc] initWithJSON:responseObject[@"recordset"][@"user"]];
+            userInfo.msg = responseObject[@"recordset"][@"msg"];
+            userInfo.msg_cn = responseObject[@"recordset"][@"msg_cn"];
+            [APPObjOnce sharedAppOnce].currentUser = userInfo;
+            
+            NSString *userToken = responseObject[@"recordset"][@"token"];
+            if(userToken != nil){
+                [[NSUserDefaults standardUserDefaults] setObject:userToken forKey:@"userToken"];
             }
-           
-        } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+            [self goToMainVC];
+        } else {
+            NSString *msg = [responseObject objectForKey:@"msg"];
+            [MTHUD showDurationNoticeHUD:msg];
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        [MTHUD hideHUD:YES completedBlock:^{
+            [MTHUD showDurationNoticeHUD:error.localizedDescription];
         }];
+    }];
 }
 
-//组件的轮数
--(NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView{
-    return 1;
+//到首页
+- (void)goToMainVC {
+    UINavigationController *vc = VCBySBName(@"mainNavVC");
+    [self.view.window setRootViewController:vc];
 }
-//返回组件行数，实际就是数据源数组的count
--(NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component{
-    return [self.pickerArray count];
-}
-//给组件设置每行的内容
--(NSString*)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component{
-    return [self.pickerArray objectAtIndex:row];
-}
-- (UIView *)pickerView:(UIPickerView *)pickerView viewForRow:(NSInteger)row forComponent:(NSInteger)component reusingView:(UIView *)view{
-    UILabel* pickerLabel = (UILabel*)view;
-    if (!pickerLabel){
-        pickerLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 0.0, 250, 30)];
-        // Setup label properties - frame, font, colors etc
-        //adjustsFontSizeToFitWidth property to YES
-        pickerLabel.textColor = UIColor.whiteColor;
-        pickerLabel.adjustsFontSizeToFitWidth = YES;
-        pickerLabel.textAlignment = NSTextAlignmentLeft;
-        pickerLabel.font = [UIFont systemFontOfSize:14];
-        
-    }
-    // Fill the label text here
-    pickerLabel.text=[self pickerView:pickerView titleForRow:row forComponent:component];
-    return pickerLabel;
-}
+
+
 
 @end
