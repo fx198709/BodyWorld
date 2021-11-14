617ff8cef553b8cd31fec3c5c4af15f8c8f498ce
feixiang <feixiang>
2021-11-14 10:24:29 +0800

    修改样式

--- fitworld/fitworld/View/TableCollectionViewCell.m
+++ fitworld/fitworld/View/TableCollectionViewCell.m
@@ -108,7 +108,7 @@
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout *)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
     
-    return CGSizeMake(220, 185);
+    return CGSizeMake(260, 185);
 }
 
 -(UICollectionReusableView *)collectionView:(UICollectionView *)collectionView viewForSupplementaryElementOfKind:(NSString *)kind atIndexPath:(NSIndexPath *)indexPath{
