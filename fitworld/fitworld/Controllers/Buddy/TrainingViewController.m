4de08a7beeaac994890b750253c70ef324473348
feixiang <feixiang>
2021-11-14 14:59:36 +0800

    修改样式

--- fitworld/fitworld/Controllers/Buddy/TrainingViewController.m
+++ fitworld/fitworld/Controllers/Buddy/TrainingViewController.m
@@ -58,7 +58,7 @@
     self.tableView.tableFooterView = [UIView new];
     self.tableView.backgroundColor = UIRGBColor(37, 37, 37, 1);
     self.tableView.layoutMargins = UIEdgeInsetsZero;
-    self.tableView.separatorColor = UIColor.grayColor;
+    self.tableView.separatorColor = UIColor.clearColor;
     [self refreshData];
     
     self.title = ChineseStringOrENFun(@"选择课程", @"CHOOSE COURSE");
@@ -103,15 +103,7 @@
         
     }
     RemoveSubviews(cell.contentView, @[]);
-    UIView *cellView = [[UIView alloc] init];
-    [cell.contentView addSubview:cellView];
-    [cellView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.right.equalTo(cell);
-        make.top.equalTo(cell).offset(0);
-        make.bottom.equalTo(cell.mas_bottom).offset(0);
-    }];
-    
-    
+    UIView *cellView = cell.contentView;
     UIView *cellBgView = [[UIView alloc] init];
     [cellBgView.layer setMasksToBounds:YES];
     [cellBgView.layer setCornerRadius:12];
@@ -119,12 +111,12 @@
     [cellBgView mas_makeConstraints:^(MASConstraintMaker *make) {
         make.left.equalTo(cellView.mas_left);
         make.right.equalTo(cellView.mas_right);
-        make.top.equalTo(cellView).offset(0);
+        make.top.equalTo(cellView).offset(7);
         make.bottom.equalTo(cellView);
     }];
 
     Course *course = dataArr[indexPath.row];
-    UIImageView *leftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 12, 56, 56)];
+    UIImageView *leftImageView = [[UIImageView alloc] initWithFrame:CGRectMake(20, 19, 56, 56)];
     [leftImageView sd_setImageWithURL:[NSURL URLWithString:[NSString stringWithFormat:@"%@%@", FITAPI_HTTPS_ROOT, course.pic]] placeholderImage:[UIImage imageNamed:@"coursedetail_top"]];
     [cellView addSubview:leftImageView];
     leftImageView.layer.cornerRadius = 28;
@@ -132,39 +124,39 @@
     
     UILabel *label1 = [[UILabel alloc] init];
     label1.text = course.name;
-    label1.font = [UIFont systemFontOfSize:13];
+    label1.font = [UIFont systemFontOfSize:15];
     label1.textColor = UIColor.whiteColor;
     [cellBgView addSubview:label1];
     [label1 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(cellBgView).offset(15);
+        make.top.equalTo(cellBgView).offset(17);
         make.left.equalTo(leftImageView.mas_right).offset(10);
         make.height.mas_equalTo(20);
         make.right.equalTo(cellBgView).offset(-90);
     }];
     
     UILabel *label2 = [[UILabel alloc] init];
-    label2.text = course.coach_name;
-    label2.font = [UIFont systemFontOfSize:11];
-    label2.textColor = UIColorFromRGB(227, 227, 227);
+    label2.text = [NSString stringWithFormat:@"%@•%@",course.coach_name,course.course_type_name];
+    label2.font = [UIFont systemFontOfSize:13];
+    label2.textColor = UIColorFromRGB(207, 207, 207);
     [cellBgView addSubview:label2];
     [label2 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(label1).offset(15);
+        make.top.equalTo(label1.mas_bottom).offset(5);
         make.left.equalTo(leftImageView.mas_right).offset(10);
         make.height.mas_equalTo(20);
         make.right.equalTo(cellBgView).offset(-90);
     }];
     
-    UILabel *label3 = [[UILabel alloc] init];
-    label3.text = course.updated_at_weekDay;
-    label3.font = [UIFont systemFontOfSize:11];
-    label3.textColor = UIColorFromRGB(227, 227, 227);
-    [cellBgView addSubview:label3];
-    [label3 mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(label2).offset(15);
-        make.left.equalTo(leftImageView.mas_right).offset(10);
-        make.height.mas_equalTo(20);
-        make.right.equalTo(cellBgView).offset(-90);
-    }];
+//    UILabel *label3 = [[UILabel alloc] init];
+//    label3.text = course.updated_at_weekDay;
+//    label3.font = [UIFont systemFontOfSize:11];
+//    label3.textColor = UIColorFromRGB(227, 227, 227);
+//    [cellBgView addSubview:label3];
+//    [label3 mas_makeConstraints:^(MASConstraintMaker *make) {
+//        make.top.equalTo(label2).offset(15);
+//        make.left.equalTo(leftImageView.mas_right).offset(10);
+//        make.height.mas_equalTo(20);
+//        make.right.equalTo(cellBgView).offset(-90);
+//    }];
     
     UIButton *selectBtn = [[UIButton alloc] init];
     selectBtn.titleLabel.font = [UIFont systemFontOfSize:14];
@@ -186,6 +178,9 @@
     }];
     selectBtn.layer.cornerRadius = 13;
     selectBtn.clipsToBounds = YES;
+    UIView *lineview = [[UIView alloc] initWithFrame:CGRectMake(20, 89.5, ScreenWidth-20, 0.5)];
+    lineview.backgroundColor = UIRGBColor(225, 225, 225, 0.5);
+    [cell.contentView addSubview:lineview];
     cell.contentView.backgroundColor = UIRGBColor(37, 37, 37, 1);
     return cell;
 }
