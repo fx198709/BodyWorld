101fe2df7783b8d7acd7972932b8f244e918e6c4
xiejc <xiejc@yiche.com>
2021-11-22 15:58:13 +0800

    update

--- fitworld/fitworld/View/ProtocolView.m
+++ fitworld/fitworld/View/ProtocolView.m
@@ -8,9 +8,11 @@
 #import "ProtocolView.h"
 
 
-@interface ProtocolView ()
+@interface ProtocolView ()<UITextViewDelegate>
 
 @property (nonatomic, weak) IBOutlet UIView *containerView;
+@property (nonatomic, weak) IBOutlet NSLayoutConstraint *containerH;
+
 @property (nonatomic, weak) IBOutlet UITextView *textView;
 @property (nonatomic, weak) IBOutlet UIButton *okBtn;
 @property (nonatomic, weak) IBOutlet UIButton *noBtn;
@@ -29,7 +31,7 @@
     [self.containerView cornerWithRadius:8.0 borderColor:self.containerView.backgroundColor];
     [self.okBtn cornerHalfWithBorderColor:self.okBtn.backgroundColor];
     [self.noBtn cornerHalfWithBorderColor:self.noBtn.backgroundColor];
-    
+        
     self.textView.delegate = self;
     
     NSString *content = @"欢迎进入FitWorld真人实时健身世界！\n点击“同意并继续”代表您已阅读并理解《用户协议》和《隐私协议》。\n本应用尊重并保护所有用户的个人隐私权，努力采取各种安全技术保护您的个人信息。当我们收集您的信息，以及获取设备某些权限的时候，我们都将单独征得您的同意。\n若您点击“不同意”，则您将无法使用我们的产品和服务，请您退出本APP。";
