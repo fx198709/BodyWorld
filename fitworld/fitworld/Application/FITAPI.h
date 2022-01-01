a4baea1f5e4bce36151a67d17b406973f4b98910
feixiang <feixiang>
2022-01-01 16:22:42 +0800

    修改了域名

--- fitworld/fitworld/Application/FITAPI.h
+++ fitworld/fitworld/Application/FITAPI.h
@@ -2,7 +2,7 @@
 #define FITAPI_h
 
 //#define FITAPI_HTTPS_PREFIX     @"https://gitee.com/api/v3/"
-#define FITAPI_HTTPS_PREFIX     @"http://1.117.70.210:8091/api/"
+#define FITAPI_HTTPS_PREFIX    @"http://m.fitworld.live/api/" //@"http://1.117.70.210:8091/api/"
 #define FITAPI_HTTPS_ROOT     @"http://1.117.70.210:8091"
 
 
